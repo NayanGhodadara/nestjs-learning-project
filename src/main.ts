b5c9312e0e68ee47ecf6f7bb544f758973ff278d
Nayan Ghodadara <nayan.uniqual@gmail.com>
2026-05-20 16:11:43 +0530

    menage prod and dev

--- src/main.ts
+++ src/main.ts
@@ -7,12 +7,17 @@ import { join } from 'path';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule) as any;
 
+  const serverUrl =
+    process.env.NODE_ENV === 'production'
+      ? 'https://test-levh.onrender.com/api/v1'
+      : 'http://localhost:1000/api/v1';
+
   const config = new DocumentBuilder()
     .setTitle('D2D API')
     .setDescription('API documentation for the D2D application')
     .setVersion('1.0')
     .addBearerAuth()
-    .addServer('https://test-levh.onrender.com/api/v1/', 'Prod server')
+    .addServer(serverUrl, 'D2d server')
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
